f35b00f0efc394ce725beb13bba2a290a13c8e0d
Yareli Chen <ychenh@miumg.edu.gt>
2023-04-01 09:39:40 -0600

    Se agrego mas filas

---
+++ program.cs
@@ -0,0 +1,11 @@
+//Variables
+int option;
+double resultado;
+string mensajee;
+
+//Dibujar menu
+Console.WriteLine("------ C A L C U L A D O R A ------");
+Console.WriteLine("1.Sumar");
+Console.WriteLine("2.Restar");
+Console.WriteLine("3.Multiplicar");
+Console.WriteLine("4.Dividir");
